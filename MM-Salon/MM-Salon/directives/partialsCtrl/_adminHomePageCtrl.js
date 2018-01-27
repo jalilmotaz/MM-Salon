50f0ab032ed4b62b69cc56b1739954d2de4f1216
Ameer <aj662@drexel.edu>
2018-01-27 17:22:07 -0500

    push image slides upload

--- MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
+++ MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
@@ -6,6 +6,9 @@
         controller: ["$scope","$window", function ($scope,$window) {
 
 
+            $scope.AllItems = [];
+            $scope.FileNameToUpload = '';
+
             $(document).ready(function () {
 
                 $('.special.cards .image').dimmer({
@@ -20,11 +23,9 @@
                 
 
             });
-               
-            
+            $scope.dateTime = new Date().getTime();
 
             var tempArr = [];
-            $scope.AllItems = [];
 
             for (var i = 0; i < $rootScope.pageModel.homePage.workGallery.length; i++) {
                 var url = $rootScope.pageModel.homePage.workGallery[i];
@@ -40,7 +41,6 @@
                 $scope.AllItems.push(tempArr);
             }
 
-            console.log($scope.AllItems);
             $scope.EditMember = function (member) {
                 $scope.memberImg = member.imgURL;
                 $scope.memberName = member.name;
@@ -48,6 +48,69 @@
                 $('.ui.modal').modal({ blurring: true }).modal('show');
             }
 
+
+
+            $scope.TriggerFileUpload = function (val) {
+                $scope.FileNameToUpload = val;
+                $("#imgUpload").trigger("click");
+            }
+
+            $scope.FileChanged = function () {
+                var files = $("#imgUpload").get(0).files;
+
+                if (files[0] != undefined) {
+                    var fu = "#imgUpload";
+                    var name = $scope.FileNameToUpload + '.jpg';
+
+                    $scope.$apply($scope.UploadImages(fu, name));
+
+                }
+   
+            }
+
+
+            $scope.UploadImages = function (fu, name) {
+
+                files = $(fu).get(0).files;
+
+                if (files[0] != undefined) {
+                    var data = new FormData();
+
+
+                    data.append("name", name);
+
+                    data.append("UploadedImage", files[0]);
+
+
+                    var ajaxRequest = $.ajax({
+                        type: "POST",
+                        url: $rootScope.server + 'api/webapi/UploadImage',
+                        contentType: false,
+                        processData: false,
+                        data: data
+                    });
+
+                    ajaxRequest.done(function (xhr, textStatus) {
+                        if (xhr == "good") {
+
+                            $rootScope.ShowToast("✔ Upload Success", "limegreen");
+                       
+                            //var tempImg = $scope.FileNameToUpload.split('/')[1];
+                            //$("#" + tempImg).attr('src', 'images/temp/' + $scope.FileNameToUpload + '.jpg');
+                        }
+                        else {
+                            alert("Error Uploading");
+                            $rootScope.ShowToast("❌ Error Uploading", "darkred");
+
+                        }
+                    });
+
+                    
+                }
+                else {
+                    $rootScope.ShowToast("No image selected", "darkred");
+                }
+            }
 
         }]
 
