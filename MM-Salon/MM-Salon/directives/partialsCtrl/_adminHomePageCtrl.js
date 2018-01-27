521a7cb189f701a8bd0ea498ca054cbebcd100aa
Ameer <aj662@drexel.edu>
2018-01-26 22:33:59 -0500

    finished admin panel layout

--- MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
+++ MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
@@ -5,7 +5,9 @@
         },
         controller: ["$scope","$window", function ($scope,$window) {
 
-            $(document).ready(function(){
+
+            $(document).ready(function () {
+
                 $('.special.cards .image').dimmer({
                     on: 'hover'
                 });
@@ -13,9 +15,39 @@
                 $('.ui.accordion').accordion();
 
                 $('.carousel').carousel('pause');
+
+
+                
+
             });
                
             
+
+            var tempArr = [];
+            $scope.AllItems = [];
+
+            for (var i = 0; i < $rootScope.pageModel.homePage.workGallery.length; i++) {
+                var url = $rootScope.pageModel.homePage.workGallery[i];
+                tempArr.push(url);
+                if ((i + 2) % 4 == 0) {
+                    $scope.AllItems.push(tempArr);
+                    tempArr = [];
+                }
+
+            }
+
+            if (tempArr.length != 0) {
+                $scope.AllItems.push(tempArr);
+            }
+
+            console.log($scope.AllItems);
+            $scope.EditMember = function (member) {
+                $scope.memberImg = member.imgURL;
+                $scope.memberName = member.name;
+                $scope.memeberAbout = member.about
+                $('.ui.modal').modal({ blurring: true }).modal('show');
+            }
+
 
         }]
 
