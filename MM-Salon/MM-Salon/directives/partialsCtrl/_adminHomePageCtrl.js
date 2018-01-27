1750caa5c6a8d7191b8872c5faa9b3e4e666333a
Ameer <aj662@drexel.edu>
2018-01-27 17:39:58 -0500

    merged

--- MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
+++ MM-Salon/MM-Salon/directives/partialsCtrl/_adminHomePageCtrl.js
@@ -3,8 +3,11 @@
         templateUrl: 'directives/partials/_adminHomePage.html',
         scope: {
         },
-        controller: ["$scope","$window", function ($scope,$window) {
+        controller: ["$scope", "$window", function ($scope, $window) {
 
+
+            $scope.AllItems = [];
+            $scope.FileNameToUpload = '';
 
             $(document).ready(function () {
 
@@ -17,14 +20,12 @@
                 $('.carousel').carousel('pause');
 
 
-                
+
 
             });
-               
-            
+            $scope.dateTime = new Date().getTime();
 
             var tempArr = [];
-            $scope.AllItems = [];
 
             for (var i = 0; i < $rootScope.pageModel.homePage.workGallery.length; i++) {
                 var url = $rootScope.pageModel.homePage.workGallery[i];
@@ -40,7 +41,76 @@
                 $scope.AllItems.push(tempArr);
             }
 
-      
+            $scope.EditMember = function (member) {
+                $scope.memberImg = member.imgURL;
+                $scope.memberName = member.name;
+                $scope.memeberAbout = member.about
+                $('.ui.modal').modal({ blurring: true }).modal('show');
+            }
+
+
+
+            $scope.TriggerFileUpload = function (val) {
+                $scope.FileNameToUpload = val;
+                $("#imgUpload").trigger("click");
+            }
+
+            $scope.FileChanged = function () {
+                var files = $("#imgUpload").get(0).files;
+
+                if (files[0] != undefined) {
+                    var fu = "#imgUpload";
+                    var name = $scope.FileNameToUpload + '.jpg';
+
+                    $scope.$apply($scope.UploadImages(fu, name));
+
+                }
+
+            }
+
+
+            $scope.UploadImages = function (fu, name) {
+
+                files = $(fu).get(0).files;
+
+                if (files[0] != undefined) {
+                    var data = new FormData();
+
+
+                    data.append("name", name);
+
+                    data.append("UploadedImage", files[0]);
+
+
+                    var ajaxRequest = $.ajax({
+                        type: "POST",
+                        url: $rootScope.server + 'api/webapi/UploadImage',
+                        contentType: false,
+                        processData: false,
+                        data: data
+                    });
+
+                    ajaxRequest.done(function (xhr, textStatus) {
+                        if (xhr == "good") {
+
+                            $rootScope.ShowToast("✔ Upload Success", "limegreen");
+
+                            //var tempImg = $scope.FileNameToUpload.split('/')[1];
+                            //$("#" + tempImg).attr('src', 'images/temp/' + $scope.FileNameToUpload + '.jpg');
+                        }
+                        else {
+                            alert("Error Uploading");
+                            $rootScope.ShowToast("❌ Error Uploading", "darkred");
+
+                        }
+                    });
+
+
+                }
+                else {
+                    $rootScope.ShowToast("No image selected", "darkred");
+                }
+            }
 
         }]
 
